f4cfe7e155bb877c265bd0eb4682b5b34856a498
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-03-14 09:32:44 -0700

    sass structure reconfigured

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import './styles/css/main.css'
+import './styles/scss/main.scss';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
