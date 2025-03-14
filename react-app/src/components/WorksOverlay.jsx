679aca7820d2d030ada19adb8595373327aed1d8
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-03-13 21:02:09 -0700

    all content added to works overlay

--- react-app/src/components/WorksOverlay.jsx
+++ react-app/src/components/WorksOverlay.jsx
@@ -1,10 +1,12 @@
 // WorksOverlay.js
 import React from 'react';
-import '../styles/overlay.css'; // Adjust the path to match your file's location
+import '../styles/overlay.css';
+import { TabList, Tabs, Tab, TabPanel } from 'react-tabs'
 
 
 function WorksOverlay({ work, onClose }) {
-    
+
+
     return (
         <div className="overlay">
             <div className="overlay-content">
@@ -37,6 +39,48 @@ function WorksOverlay({ work, onClose }) {
                 {work.acf?.overview_description && (
                     <p>{work.acf.overview_description}</p>
                 )}
+
+                {work.acf?.["work-tabs"]?.length > 0 && ( // Only render tabs if they exist
+                    <Tabs>
+                        {/* Render Tab Titles */}
+                        <TabList>
+                            {work.acf["work-tabs"].map((tab, index) => (
+                                <Tab key={index}>{tab.tab_title}</Tab>
+                            ))}
+                        </TabList>
+
+                        {/* Render Tab Content */}
+                        {work.acf["work-tabs"].map((tab, index) => (
+                            <TabPanel key={index}>
+                                {tab.tab_content?.map((contentItem, i) => (
+                                    <div key={i} className="tab-content-item">
+                                        {/* Handle "Text_Feature" Layout */}
+                                        {contentItem.acf_fc_layout === "Text_Feature" && (
+                                            <div>
+                                                <h4>{contentItem.feature_title}</h4>
+                                                <p>{contentItem.feature_description}</p>
+                                            </div>
+                                        )}
+
+                                        {/* Handle "Key_Takeaways" Layout */}
+                                        {contentItem.acf_fc_layout === "Key_Takeaways" && (
+                                            <div>
+                                                <h4>{contentItem.title}</h4>
+                                                <div
+                                                    dangerouslySetInnerHTML={{
+                                                        __html: contentItem.description,
+                                                    }}
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                ))}
+                            </TabPanel>
+                        ))}
+                    </Tabs>
+                )}
+
+                
 
                 
             </div>
