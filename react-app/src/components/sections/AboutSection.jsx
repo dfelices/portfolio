7bed9918e7a7668fae747cbae05b619bc9c2fd74
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-03-15 14:41:45 -0700

    chip styling applied to about section fun-stuff items

--- react-app/src/components/sections/AboutSection.jsx
+++ react-app/src/components/sections/AboutSection.jsx
@@ -8,14 +8,14 @@ function AboutSection (){
     if (error) return <p>Error: {error}</p>;
     
     return(
-        <section id='about' className='about-section'>
+        <section id='about'>
             <h2>About</h2>
             <p>{restData.personal_bio}</p>
 
             {restData.fun_stuff && restData.fun_stuff.length > 0 ? (
-                <ul>
+                <ul className='fun-list'>
                     {restData.fun_stuff.map((itemObj, index) => (
-                        <li key={index}>{itemObj.item}</li>
+                        <li key={index} className='fun'>{itemObj.item}</li>
                     ))}
                 </ul>
             ) : (
