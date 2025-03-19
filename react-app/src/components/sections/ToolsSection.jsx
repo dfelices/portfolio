bc81764a612fe8c7f9e086411ac03e26f7709478
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-03-19 14:24:06 -0700

    added conditional rendering for tools tab

--- react-app/src/components/sections/ToolsSection.jsx
+++ react-app/src/components/sections/ToolsSection.jsx
@@ -2,10 +2,13 @@ import { restBase, useFetch } from '../../utilities/Globals'
 import Loading from "../../utilities/Loading"
 import ProfessionalBio from '../professionalBio'
 import { TabList, Tabs, Tab, TabPanel } from 'react-tabs'
+import { useMediaQuery } from 'react-responsive'
 
 
 function ToolsSection (){
     const { data: restData, isLoading, error } = useFetch(restBase + 'portfolio-tool-category?per_page=100');
+
+    const isAbove480 = useMediaQuery({ minWidth: 481 });
 
     if (isLoading) return <Loading />;
     if (error) return <p>Error: {error}</p>;
@@ -13,7 +16,7 @@ function ToolsSection (){
     // Helper function to filter categories
     const getAllTerms = () => 
         restData.filter(
-            (term) => !["All", "Design", "Development", "Professional Highlights"].includes(term.name)
+            (term) => !["All", "Design", "Development", "Qualities"].includes(term.name)
         );
     
     const getTermsByParent = (parentName) => {
@@ -24,7 +27,7 @@ function ToolsSection (){
     // Filter terms by category
     const designTerms = getTermsByParent("Design")
     const developmentTerms = getTermsByParent("Development")
-    const professionalHighlightsTerms = getTermsByParent("Professional Highlights")
+    const qualitiesTerms = getTermsByParent("Qualities")
 
     return(
         <section id='tools'>
@@ -33,19 +36,22 @@ function ToolsSection (){
                 <ProfessionalBio />
                 <Tabs>
                     <TabList>
-                        <Tab>All</Tab>
+                        {isAbove480 && <Tab>All</Tab>}
                         <Tab>Design</Tab>
-                        <Tab>Development</Tab>
-                        <Tab>Professional Highlights</Tab>
+                        <Tab> {isAbove480? 'Development' : 'Dev'} </Tab>
+                        <Tab>Qualities</Tab>
                     </TabList>
 
-                    <TabPanel>
-                        <ul className='tools-scroll'>
-                            {getAllTerms().map((term) => (
-                                <li key={term.id} className='tool'>{term.name}</li>
-                                ))}
-                        </ul>
-                    </TabPanel>
+                    {isAbove480 && (
+                        <TabPanel>
+                            <ul className='tools-scroll'>
+                                {getAllTerms().map((term) => (
+                                    <li key={term.id} className='tool'>{term.name}</li>
+                                    ))}
+                            </ul>
+                        </TabPanel>
+                    )}
+
                     <TabPanel>
                         <ul className='tools-scroll'>
                             {designTerms.map((term) => (
@@ -62,7 +68,7 @@ function ToolsSection (){
                     </TabPanel>
                     <TabPanel>
                         <ul className='tools-scroll'>
-                            {professionalHighlightsTerms.map((term) => (
+                            {qualitiesTerms.map((term) => (
                                 <li key={term.id} className='tool'>{term.name}</li>
                                 ))}
                         </ul>
