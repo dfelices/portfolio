b21f800bf6c4ded70d46d2597cec803880963bee
Danielle Felices <141359239+dfelices@users.noreply.github.com>
2025-03-26 22:53:07 -0700

    added accessibility features to desktop nav

--- react-app/src/components/navigation/DesktopNav.jsx
+++ react-app/src/components/navigation/DesktopNav.jsx
@@ -23,15 +23,24 @@ function DesktopNav() {
 
     return (
         <>
+            <a href="#main-content" className="skip-link">Skip to main content</a>
             <nav className='desktop-site-navigation' role="navigation" aria-label="Main site navigation">
                 <ul>
-                    <li >
+                    <li role="listitem">
                         <Link 
                         smooth to='/#work' 
                         className='desktop-nav-link'
+                        aria-current={activeSection === 'work' ? 'page' : undefined}
                         onClick={() => setActiveSection('work')}
                         >
-                            <svg height="28" width="28" xmlns="http://www.w3.org/2000/svg" className='desktop-nav-link' aria-hidden="true">
+                            <svg 
+                            height="28" 
+                            width="28" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            className='desktop-nav-link' 
+                            aria-hidden="true"
+                            focusable="false"
+                            >
                                 <circle r="12" cx="14" cy="14" fill="#FFFFFF" stroke="#011627" strokeWidth="3" />
                             </svg>
 
@@ -39,14 +48,21 @@ function DesktopNav() {
                         </Link>
                         
                     </li>
-                    <li>
+                    <li role="listitem">
                         <Link 
                         smooth to='/#tools' 
                         className='desktop-nav-link'
+                        aria-current={activeSection === 'tools' ? 'page' : undefined}
                         onClick={() => setActiveSection('tools')}
                         >
-
-                            <svg height="28" width="28" xmlns="http://www.w3.org/2000/svg" className='desktop-nav-icon' aria-hidden="true">
+                            <svg 
+                            height="28" 
+                            width="28" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            className='desktop-nav-icon' 
+                            aria-hidden="true"
+                            focusable="false"
+                            >
                                 <circle r="12" cx="14" cy="14" fill="#FFFFFF" stroke="#011627" strokeWidth="3" />
                             </svg>
                             
@@ -54,14 +70,22 @@ function DesktopNav() {
 
                         </Link>
                     </li>
-                    <li>
+                    <li role="listitem">
                         <Link 
                         smooth to='/#about' 
                         className='desktop-nav-link'
+                        aria-current={activeSection === 'about' ? 'page' : undefined}
                         onClick={() => setActiveSection('about')}
                         
                         >
-                            <svg height="28" width="28" xmlns="http://www.w3.org/2000/svg" className='desktop-nav-icon' aria-hidden="true">
+                            <svg 
+                            height="28" 
+                            width="28" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            className='desktop-nav-icon' 
+                            aria-hidden="true"
+                            focusable="false"
+                            >
                                 <circle r="12" cx="14" cy="14" fill="#FFFFFF" stroke="#011627" strokeWidth="3" />
                             </svg>
                                 
